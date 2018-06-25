e4efd0adf151aa8c717f3767dbe79cc39b5d256c
RebelRed <rgdemstar@live.com>
2018-06-25 13:11:24 +0530

    ajax

--- index.js
+++ index.js
@@ -15,43 +15,99 @@ $('#Search-btn-2').on('click', (e) => {
   e.preventDefault();
 })
 
-function getMoviesByID(searchID){
-  axios.get('http://www.omdbapi.com/?i='+searchID+'&apikey=460ecbb6')
-  .then((response) => {
-    console.log(response);
-    if(response.data.Response == "True"){
-      let movies = response.data;
-    let output = `<div class="col-sm-12 col-md-6 col-lg-4 col-xs-12">
-    <div class="card" style="width: 22rem; margin: 3%;">
-      <img class="card-img-top" src="${movies.Poster}" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${movies.Title}</h5>
-        <p class="card-text">${movies.Year}</p>
-        <a href="#" class="btn btn-primary" onclick="movieSelected('${movies.imdbID}'">Movie Details</a>
-      </div>
-    </div>
-    </div>`;
+// function getMoviesByID(searchID){
+//   axios.get('http://www.omdbapi.com/?i='+searchID+'&apikey=460ecbb6')
+//   .then((response) => {
+//     console.log(response);
+//     if(response.data.Response == "True"){
+//       let movies = response.data;
+//     let output = `<div class="col-sm-12 col-md-6 col-lg-4 col-xs-12">
+//     <div class="card" style="width: 22rem; margin: 3%;">
+//       <img class="card-img-top" src="${movies.Poster}" alt="Card image cap">
+//       <div class="card-body">
+//         <h5 class="card-title">${movies.Title}</h5>
+//         <p class="card-text">${movies.Year}</p>
+//         <a href="#" class="btn btn-primary" onclick="movieSelected('${movies.imdbID}'">Movie Details</a>
+//       </div>
+//     </div>
+//     </div>`;
 
-    $('#movies').html(output);
-    }else{
-      let Err = response.data
-      let output = `<div class="col-sm-12 col-md-6 col-lg-4 col-xs-12">
-      <div class="card" style="width: 22rem; margin: 3%;">
-        <img class="card-img-top" src="./noimagem.jpg" alt="Card image cap">
-        <div class="card-body">
-          <strong><h4 class="card-title">${Err.Error}</h4></strong>
-        </div>
-      </div>
-      </div>`;
-      $('#movies').html(output);
-    }
+//     $('#movies').html(output);
+//     }else{
+//       let Err = response.data
+//       let output = `<div class="col-sm-12 col-md-6 col-lg-4 col-xs-12">
+//       <div class="card" style="width: 22rem; margin: 3%;">
+//         <img class="card-img-top" src="./noimagem.jpg" alt="Card image cap">
+//         <div class="card-body">
+//           <strong><h4 class="card-title">${Err.Error}</h4></strong>
+//         </div>
+//       </div>
+//       </div>`;
+//       $('#movies').html(output);
+//     }
     
-  }).catch((err) => {
-    console.log(err)
+//   }).catch((err) => {
+//     console.log(err)
 
-    alert(err)
-  });
+//     alert(err)
+//   });
+// }
+
+function getMoviesByID(searchID){
+
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    async: true,
+    url:'http://www.omdbapi.com/?i='+searchID+'&apikey=460ecbb6',
+
+    success: (response) => {
+
+      console.log(response);
+      if(response.Response == "True"){
+        let movies = response;
+        let output = `<div class="col-sm-12 col-md-6 col-lg-4 col-xs-12">
+        <div class="card" style="width: 22rem; margin: 3%;">
+          <img class="card-img-top" src="${movies.Poster}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${movies.Title}</h5>
+            <p class="card-text">${movies.Year}</p>
+            <a href="#" class="btn btn-primary" onclick="movieSelected('${movies.imdbID}'">Movie Details</a>
+          </div>
+        </div>
+        </div>`;
+        $('#movies').html(output);
+      }else{
+        let error = response.Error 
+
+        let Err = response
+        let output = `<div class="col-sm-12 col-md-6 col-lg-4 col-xs-12">
+        <div class="card" style="width: 22rem; margin: 3%;">
+          <img class="card-img-top" src="./noimagem.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${Err.Error}</h5>
+          </div>
+        </div>
+        </div>`;
+        $('#movies').html(output);
+      }
+      
+    },
+    error: (err) => {
+       console.log(err)
+    
+    }
+
+  })
+
+
 }
+
+
+
+
+
+
 function getMovies(searchText, searchYear){
 
   $.ajax({
